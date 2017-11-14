80b5559a89f324ad05ad11caf0f83acaa0ab59c0
Oleksiy Shashlyuk <alexkillgur@gmail.com>
2017-11-14 22:46:52 +0200

    hw_04: Use DictWriter

--- HT_4/hw_04.py
+++ HT_4/hw_04.py
@@ -16,26 +16,18 @@ def parse_line(line):
     # parse date_time
     match = re.search(r'(?P<date_time>\d{4}[-/]\d{2}[-/]\d{2} \d{2}:\d{2}:\d{2})', line)
     date_time = match.group("date_time")
-
     # parse marker
     marker = re.findall(r"(?=("+'|'.join(MARKERS)+r"))", line)[0]
-
     # parse description
     description = re.sub(r'^\W*\w+\W*', '', line.split(marker)[-1].strip())
-
     return date_time, marker, description
-
-
-# with open('all_data.csv', 'w') as csvfile:
-    # file_writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    # file_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    # file_writer = csv.writer(csvfile, delimiter=',', quotechar='|')
-    # file_writer = csv.writer(csvfile, delimiter=',')
 
 filename = "../HT_4/report/all_data.csv"
 os.makedirs(os.path.dirname(filename), exist_ok=True)
-
-file_writer = csv.writer(open(filename, "w", newline=''), delimiter=';')
+# file_writer = csv.writer(open(filename, "w", newline=''), delimiter=';')
+field_names = ['line_id', 'marker', 'date_time', 'description']
+file_writer = csv.DictWriter(open(filename, "w", newline=''), delimiter=';', fieldnames=field_names)
+file_writer.writeheader()
 
 with open("openerp-server.log", "r") as log_file:
     line_id = 0
@@ -43,10 +35,23 @@ with open("openerp-server.log", "r") as log_file:
         line_id += 1
         log_line = log_line.strip()
         if is_marker_exist(log_line):
-            # print(log_line)
             date_time, marker, description = parse_line(log_line)
-            # print(str(line_id) + " " + str(date_time) + " " + str(marker) + " " + str(description))
-            file_writer.writerow([line_id, marker, date_time, description])
+            # file_writer.writerow([line_id, marker, date_time, description])
+            file_writer.writerow({'line_id': line_id, 'marker': marker, 'date_time': date_time, 'description': description})
+
+
+# import csv
+#
+# seen = [] # or set()
+# dup_scan_col = 3
+# uniques = []
+#
+# with open('yourfile.csv', 'r') as f:
+#     reader = csv.reader(f, delimiter='\t')
+#     for row in reader:
+#        if row[dup_scan_col] not in seen:
+#           uniques.append(row)
+#           seen.append(row[dup_scan_col])
 
 
 
