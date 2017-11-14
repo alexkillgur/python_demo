7af6dbcfdf5d0dbbc6d415d96211785829bafa41
Oleksiy Shashlyuk <alexkillgur@gmail.com>
2017-11-15 01:16:09 +0200

    hw_04: Count unique lines and write them to unique.csv

--- HT_4/hw_04.py
+++ HT_4/hw_04.py
@@ -1,8 +1,6 @@
-import operator
-import re
 import csv
 import os
-import itertools
+import re
 
 MARKERS = ("WARNING", "ERROR", "CRITICAL")
 
@@ -24,13 +22,30 @@ def parse_line(line):
     description = re.sub(r'^\W*\w+\W*', '', line.split(marker)[-1].strip())
     return date_time, marker, description
 
-filename = "../HT_4/report/all_data.csv"
-os.makedirs(os.path.dirname(filename), exist_ok=True)
 
+def get_dict(line_id, date_time, marker, description):
+    return {'line_id': line_id,
+            'marker': marker,
+            'date_time': date_time,
+            'description': description}
+
+
+def count(all_lines, line):
+    counter = 0
+    for current_line in all_lines:
+        if current_line['description'] == line:
+            counter += 1
+    return counter
+
+# create all_data.csv
+filename_for_all = "../HT_4/report/all_data.csv"
+os.makedirs(os.path.dirname(filename_for_all), exist_ok=True)
 field_names = ['line_id', 'marker', 'date_time', 'description']
-file_writer = csv.DictWriter(open(filename, "w", newline=''),
-                             delimiter=';', fieldnames=field_names)
+file_writer = csv.DictWriter(open(filename_for_all, "w", newline=''), delimiter=';', fieldnames=field_names)
 file_writer.writeheader()
+
+# array for counting duplicates
+all_lines = []
 
 # write all_data.csv by markers
 with open("openerp-server.log", "r") as log_file:
@@ -39,22 +54,29 @@ with open("openerp-server.log", "r") as log_file:
         line_id += 1
         log_line = log_line.strip()
         if is_marker_exist(log_line):
+            # get parsing parameters
             date_time, marker, description = parse_line(log_line)
-            file_writer.writerow({'line_id': line_id,
-                                  'marker': marker,
-                                  'date_time': date_time,
-                                  'description': description})
+            # get dictionary to write
+            dictionary = get_dict(line_id, date_time, marker, description)
+            file_writer.writerow(dictionary)
+            # fill array for counting duplicates
+            all_lines.append(dictionary)
 
-set_of_description = set()
-unique_lines = []
+# array for unique description for counting duplicates
+set_of_description = []
 
-file_reader = csv.DictReader(open(filename, 'r', newline=''),
-                             delimiter=';', fieldnames=field_names)
+# create unique.csv
+filename_for_unique = "../HT_4/report/unique.csv"
+field_names = ['count', 'marker', 'date_time', 'description']
+file_writer = csv.DictWriter(open(filename_for_unique, "w", newline=''), delimiter=';', fieldnames=field_names)
+file_writer.writeheader()
+
+# read all_data.csv and write unique.csv
+file_reader = csv.DictReader(open(filename_for_all, 'r', newline=''), delimiter=';')
 for row in file_reader:
     if row['description'] not in set_of_description:
-        print(row['line_id'])
-        unique_lines.append(row)
-        set_of_description.add(row['description'])
-
-print(set_of_description)
-print(unique_lines)
+        set_of_description.append(row['description'])
+        file_writer.writerow({'count': count(all_lines, row['description']),
+                              'marker': row['marker'],
+                              'date_time': row['date_time'],
+                              'description': row['description']})
